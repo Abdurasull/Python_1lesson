6aefaf75497ae32830b0915be6f0865d803c791b
Abdurasull <abdurasulsalimov94@gmail.com>
2024-12-23 20:21:34 +0500

    1-misol.py bajarildi

---
+++ 1-misol.py
@@ -0,0 +1,13 @@
+int_a = 25#int turidagi ma`lumotni kiritamiz 
+
+float_a = 3.14#float turidagi ma`lumot kiritamiz
+
+strint_a = "Salom, dunyo!"#string turidagi ma`lumot kiritamiz
+
+boolen_a = True#boolen turidagi ma`lumot kiritamiz
+
+#turni aniqlash uchun type() function dan foydalanamiz
+print(type(int_a))
+print(type(float_a))
+print(type(strint_a))
+print(type(boolen_a))
